516de3649040e84ffd5e68f2d4d0d5eb9bec3b0a
Simon Johnsson <73998667+BAMF0@users.noreply.github.com>
2024-03-26 13:58:35 +0100

    Initial commit VS

---
+++ GameOfLife3D/GameOfLife3D.cpp
@@ -0,0 +1,12 @@
+﻿// GameOfLife3D.cpp : Defines the entry point for the application.
+//
+
+#include "GameOfLife3D.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
